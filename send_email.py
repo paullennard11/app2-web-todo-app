db6226ac22b3439f76a660a385307f76816e5e87
Lennard <lennardpolito@gmail.com>
2023-08-27 17:27:33 +0800

    update to secure pass

--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "lennadpolito@gmail.com"
-    password = "gemencahkqvmjfgi"
+    password = os.getenv("PASSWORD")
 
     receiver = "lennardpolito@gmail.com"
     context = ssl.create_default_context()
